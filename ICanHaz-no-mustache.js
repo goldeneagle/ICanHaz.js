8f1d9463d96dd12c098ae6bd0236c31efb090cfb
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-20 12:50:26 +0200

    Rebuild with new options

--- ICanHaz-no-mustache.js
+++ ICanHaz-no-mustache.js
@@ -15,17 +15,21 @@ More info at: http://icanhazjs.com
     var ich = {
         VERSION: "0.9",
         templates: {},
-        
+
         // grab jquery or zepto if it's there
         $: (typeof window !== 'undefined') ? window.jQuery || window.Zepto || null : null,
-        
+
+        allowReload: false,
+
         // public function for adding templates
         // We're enforcing uniqueness to avoid accidental template overwrites.
         // If you want a different template, it should have a different name.
         addTemplate: function (name, templateString) {
-            if (ich[name]) throw "Invalid name: " + name + ".";
-            if (ich.templates[name]) throw "Template \" + name + \" exists";
-            
+            if (!ich.allowReload) {
+                if (ich[name]) throw "Invalid name: " + name + ".";
+                if (ich.templates[name]) throw "Template \" + name + \" exists";
+            }
+
             ich.templates[name] = templateString;
             ich[name] = function (data, raw) {
                 data = data || {};
@@ -33,7 +37,7 @@ More info at: http://icanhazjs.com
                 return (ich.$ && !raw) ? ich.$(result) : result;
             };
         },
-        
+
         // clears all retrieval functions and empties caches
         clearAll: function () {
             for (var key in ich.templates) {
@@ -41,21 +45,21 @@ More info at: http://icanhazjs.com
             }
             ich.templates = {};
         },
-        
+
         // clears/grabs
         refresh: function () {
             ich.clearAll();
             ich.grabTemplates();
         },
-        
+
         // grabs templates from the DOM and caches them.
         // Loop through and add templates.
-        // Whitespace at beginning and end of all templates inside <script> tags will 
-        // be trimmed. If you want whitespace around a partial, add it in the parent, 
+        // Whitespace at beginning and end of all templates inside <script> tags will
+        // be trimmed. If you want whitespace around a partial, add it in the parent,
         // not the partial. Or do it explicitly using <br/> or &nbsp;
-        grabTemplates: function () {        
-            var i, 
-                scripts = document.scripts, 
+        grabTemplates: function () {
+            var i,
+                scripts = document.scripts,
                 l = scripts.length,
                 script,
                 trash = [];
@@ -71,7 +75,7 @@ More info at: http://icanhazjs.com
             }
         }
     };
-    
+
     // attach it to the window
     if (typeof require !== 'undefined') {
         module.exports = ich;
@@ -79,7 +83,7 @@ More info at: http://icanhazjs.com
         // else make global
         window.ich = ich;
     }
-    
+
     if (typeof document !== 'undefined') {
         if (ich.$) {
             ich.$(function () {
@@ -91,6 +95,6 @@ More info at: http://icanhazjs.com
             });
         }
     }
-        
+
 })()
 })();
